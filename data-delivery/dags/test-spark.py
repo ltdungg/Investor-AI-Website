9f5987dac7a76016e11350e42b7e72f70ff76d64
ltdungg <dungluong54321@gmail.com>
2025-03-30 23:36:03 +0700

    Add backend dockerfile and fix frontend dockerfile

--- data-delivery/dags/test-spark.py
+++ data-delivery/dags/test-spark.py
@@ -1,51 +1,61 @@
+from datetime import timedelta
+
 import pandas as pd
 from pyspark.sql import SparkSession
 from airflow.decorators import dag, task
 from pyspark.sql.types import *
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+BUCKET = "investor-ai-bucket"
 
 @dag(
+    dag_id="load_postgres_data_to_minio",
     schedule=None,
     catchup=False,
-    tags=["example"],
+    default_args={
+        'depends_on_past': False,
+        'retries': 0,
+    }
 )
-def example_pyspark():
-    @task()
-    def spark_task():
-        spark = SparkSession.builder \
+def load_postgres_data_to_minio():
+    spark = SparkSession.builder \
             .appName("test") \
             .master("spark://spark-master:7077") \
             .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4,"
-                                         + "com.amazonaws:aws-java-sdk-bundle:1.12.262") \
+                                         + "com.amazonaws:aws-java-sdk-bundle:1.12.262,"
+                                         + "org.postgresql:postgresql:42.7.5") \
             .config("spark.hadoop.fs.s3a.endpoint", "http://minio:9000") \
-            .config("spark.hadoop.fs.s3a.access.key", "xO9Vm8MljqPj1iN7cgsd") \
-            .config("spark.hadoop.fs.s3a.secret.key", "Tmj16j86CPmwYFPpToHIYVPPoizRK1oXWg2AKfdl") \
+            .config("spark.hadoop.fs.s3a.access.key", "36Dr1hhdZcDMGHJ7GTYL") \
+            .config("spark.hadoop.fs.s3a.secret.key", "kK0i0YpNLsYIVC2bR6qOG5hMI0ZQBvUFlPTm9I9M") \
             .config("spark.hadoop.fs.s3a.path.style.access", "true") \
             .config("fs.s3a.connection.ssl.enabled", "false") \
             .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
             .getOrCreate()
+    
+    @task()
+    def read_and_load_to_minio():
+        postgres_url = "jdbc:postgresql://postgres:5432/airflow"
+        postgres_properties = {
+            "user": "airflow",
+            "password": "airflow",
+            "driver": "org.postgresql.Driver"
+        }
+        df = spark.read.jdbc(url=postgres_url, table="stock.stock_price", properties=postgres_properties)
 
-        data = [("Alice", 25), ("Bob", 30), ("Charlie", 28)]
-
-        # Định nghĩa schema
-        schema = StructType([
-            StructField("name", StringType(), True),
-            StructField("age", IntegerType(), True)
-        ])
-
-        # Tạo DataFrame
-        df = spark.createDataFrame(data, schema)
-
-        df.write.mode("overwrite").csv("s3a://test-bucket/test/")
+        df.write \
+            .partitionBy("symbol") \
+            .mode("overwrite") \
+            .parquet("s3a://{}/RAW_STOCK_DATA".format(BUCKET))
+        return "Success writing to MinIO"
 
 
     @task
-    def print_df():
+    def success():
         print("Done")
 
-    spark_task()
-    print_df()
+    read_and_load_to_minio()
+    success()
 
-
-example_pyspark()
+load_postgres_data_to_minio()
 
 
