9dd498421a38fb86bf4773938726e23fc457fa6e
Nguyễn Hải Hiếu <nguyenhaihieu89@gmail.com>
2025-04-04 22:37:43 +0700

    update: api.js, stock graph

--- frontend/src/utils/api/Api.js
+++ frontend/src/utils/api/Api.js
@@ -1,40 +1,46 @@
 import axios from "axios";
-import { isVariable } from "../jwtUtils";
 import { urlBackend, jwtTagStorage } from "../const/Global";
 
 const loginUrl = "/login";
 
 const api = axios.create({
-    baseURL: urlBackend, // URL của backend
-    headers: { "Content-Type": "application/json" },
+  baseURL: urlBackend, // URL của backend
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = window.localStorage.getItem(jwtTagStorage);
+  (config) => {
+    const token = window.localStorage.getItem(jwtTagStorage);
 
-        if (token && isVariable(token)) {
-            config.headers.Authorization = `Bearer ${token}`;
-            return config;
-        } else {
-            window.location.href = loginUrl; // Điều hướng về trang đăng nhập
-        }
-    },
-    (error) => {
-        return Promise.reject(error);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response) {
-            console.log("Token hết hạn! Chuyển về trang login...");
-            window.localStorage.removeItem(jwtTagStorage); // Xoá token
-            window.location.href = loginUrl; // Điều hướng về trang đăng nhập
-            return error;
-        }
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const responseCode = error.response.status;
+      const responseData = error.response.data;
+    //   const requiresAuth = error.config?.requiresAuth; // Lấy cờ tùy chỉnh
+
+      if (responseCode === 401 || responseCode == 403) {
+        console.log("Token hết hạn! Chuyển về trang login...");
+        window.localStorage.removeItem(jwtTagStorage); // Xoá token
+        window.location.href = loginUrl; // Điều hướng về trang đăng nhập
+      } else {
+        console.log(responseData);
+        alert(responseData);
+      }
     }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
