b9220368dca3a0a022e75d9c827dae0180d364ab
ltdungg <dungluong54321@gmail.com>
2025-05-06 22:01:13 +0700

    Modify: Analysis Reports Page

--- frontend/src/components/analysis_report/StockBox/StockBox.jsx
+++ frontend/src/components/analysis_report/StockBox/StockBox.jsx
@@ -6,8 +6,8 @@ const StockBox = ({ symbol, name, source, date, onClick }) => {
     <div className="stock-box" onClick={onClick}>
       <div className="stock-code">{symbol}</div>
       <div className="stock-title">{name}</div>
-      <div className="stock-source">Source: {source}</div>
-      <div className="stock-date">Published: {date}</div>
+      <div className="stock-source">Nguồn: {source}</div>
+      <div className="stock-date">Ngày đăng: {date}</div>
     </div>
   );
 };
