ee8a573bc7131743a63df3ef70e26ce98579352c
ltdungg <dungluong54321@gmail.com>
2025-05-08 23:27:34 +0700

    Modify: replace to append

--- Stock_LSTM_Torch/data/load_posgres.py
+++ Stock_LSTM_Torch/data/load_posgres.py
@@ -6,7 +6,7 @@ postgres_connection_url = 'postgresql+psycopg2://airflow:airflow@postgres/airflo
 def load_df_to_postgres(table, schema, df):
 
     engine = create_engine(postgres_connection_url)
-    df.to_sql(table, con=engine, if_exists='replace', index=False, schema=schema, chunksize=10000)
+    df.to_sql(table, con=engine, if_exists='append', index=False, schema=schema, chunksize=10000)
     print(f"Successfully load data to {schema}.{table}")
 
     return
